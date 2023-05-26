41d821c9ec8e1f3e46516fe2a2183058f84026d2
criptorcronom <94016869+criptorcronom@users.noreply.github.com>
2023-05-26 18:19:19 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+var btn = document.querySelector(".no");
+var position
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    btn.addEventListener("touchstart", function() {
+        position ? (position = 0) : (position = 180);
+        btn.style.transform = `translate(${position}px,0px)`;
+        btn.style.transition = "all 0.3s ease";
+      });
+      
+      btn.addEventListener("touchend", function() {
+        position ? (position = 0) : (position = 180);
+        btn.style.transform = `translate(${position}px,0px)`;
+        btn.style.transition = "all 0.3s ease";
+      });
+  } else {
+    btn.addEventListener("mouseover", function() {
+        position ? (position = 0) : (position = 180);
+        btn.style.transform = `translate(${position}px,0px)`;
+        btn.style.transition = "all 0.4s ease";
+        });
+  }  
